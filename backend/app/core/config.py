9295c64ffcc868b9d1fd693a915ebb2074c45ccc
EslamKamel89 <eslam_kamel_89@hotmail.com>
2026-04-19 07:53:47 +0200

    feat(config): add PostgreSQL settings and construct DATABASE_URL property

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -9,11 +9,24 @@ class Settings(BaseSettings):
     API_VERSION: str
     SITE_NAME: str
     ENVIRONMENT: Literal["local", "staging", "production"] = "local"
+    POSTGRES_USER: str
+    POSTGRES_PASSWORD: str
+    POSTGRES_DB: str
+    POSTGRES_HOST: str
+    POSTGRES_PORT: int
+    POSTGRES_DRIVER: str = "asyncpg"
     model_config = SettingsConfigDict(
         env_file=".envs/.env.local",
         env_ignore_empty=True,
         extra="ignore",
     )
+
+    @property
+    def DATABASE_URL(self) -> str:
+        return (
+            f"postgresql+{self.POSTGRES_DRIVER}://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}"
+            f"@{self.POSTGRES_HOST}:{self.POSTGRES_PORT}/{self.POSTGRES_DB}"
+        )
 
 
 settings = Settings()  # type: ignore
